4b1d48735f589bd421cc21c3164484e319f5e962
Ryan Fong <ryan.fong@sbcglobal.net>
2017-06-05 18:34:32 -0700

    Finish Part 1: ControllerBase, Session, and Router

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -7,36 +7,70 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
     @req = req
     @res = res
+    @already_built_response = false
+    @params = route_params.merge(req.params)
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if already_built_response?
+      raise "Cannot render twice"
+    else
+      @res.header["Location"] = url
+      @res.status = 302
+      @already_built_response = true
+    end
+    session.store_session(@res)
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    @res[content_type] = content
+    if already_built_response?
+      raise "Cannot render twice"
+    else
+      @res['Content-Type'] = content_type
+      @res.write(content)
+      @already_built_response = true
+    end
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(template_fname)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
